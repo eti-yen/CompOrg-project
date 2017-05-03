550f43bbc89714f279c249ecfac85e243a6e3594
eti-yen <emeraldwatch15@gmail.com>
2017-05-03 20:36:32 +0100

    Improved pipeline

--- code/iplc-sim.c
+++ code/iplc-sim.c
@@ -72,7 +72,7 @@ unsigned int branch_count=0;
 unsigned int correct_branch_predictions=0;
 
 unsigned int debug=0;
-unsigned int dump_pipeline=1;
+unsigned int dump_pipeline=0;
 
 enum instruction_type {NOP, RTYPE, LW, SW, BRANCH, JUMP, JAL, SYSCALL};
 
@@ -416,6 +416,7 @@ void iplc_sim_push_pipeline_stage()
 	
 	if (inserted_nop) {
 	    pipeline[WRITEBACK] = pipeline[MEM];
+	    instruction_count++;
 	    bzero(&(pipeline[MEM]), sizeof(pipeline_t));
 	}
     }
